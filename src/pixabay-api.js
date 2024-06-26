c59fb7d253b632a9e1dddf6fe5e86783970f14cf
bjpangulayan <bjspangulayan23@gmail.com>
2024-06-26 10:53:09 +0800

    Update pixabay-api.js

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '43695781-37520996cf2f13ffb5a7a3fb5';
+const API_KEY = '43730897-75789e447f4fce7bddaa4c488';
 
 export const getAPI = async (query, page) => {
   const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
